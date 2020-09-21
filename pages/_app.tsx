10222cc7bdf98018676ebe1237851177b0c30c1d
Åsa Gustafsson <asa.m.gustafsson@gmail.com>
2020-09-21 15:40:04 +0200

    project setup: typescript, less, prettier start

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+//import '../styles/globals.css'
+import '../styles/globals.less'
+
+const MyApp = ({ Component, pageProps }) => {
+    return (
+        <Component {...pageProps} />
+    );
+}
+
+export default MyApp;
